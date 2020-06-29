b7377ac95d1c6beb24c9186132b32c27ca5bf5ec
Gijs van Lammeren <gijs@starred.com>
2020-06-29 09:15:30 +0200

    Revert "Fix broken framework test."
    This reverts commit dc18b4c1eda4975389cfa15b45843192f104e807.

--- src/DependencyInjection/BrefMessengerExtension.php
+++ src/DependencyInjection/BrefMessengerExtension.php
@@ -2,28 +2,16 @@
 
 namespace Bref\Symfony\Messenger\DependencyInjection;
 
-use Bref\Symfony\Messenger\Service\Sqs\SqsTransportFactory;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
-class BrefMessengerExtension extends Extension implements CompilerPassInterface
+class BrefMessengerExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yaml');
-    }
-
-    public function process(ContainerBuilder $container): void
-    {
-        if ($container->hasDefinition('messenger.transport.sqs.factory')) {
-            $definition = new Definition(SqsTransportFactory::class);
-            $definition->setDecoratedService('messenger.transport.sqs.factory');
-            $definition->addArgument('@.inner');
-        }
     }
 }

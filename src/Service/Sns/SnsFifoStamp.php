184b50e7d62154454f3ae6931e101d524feb225f
Eric <eric.robert@simpleclinic.net>
2023-10-19 16:24:46 +1000

    Adds SnsFifoStamp class based of the Symfony base stamp for SQS and applies when the sns topic contains .fifo

---
+++ src/Service/Sns/SnsFifoStamp.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Bref\Symfony\Messenger\Service\Sns;
+
+use Symfony\Component\Messenger\Stamp\NonSendableStampInterface;
+
+class SnsFifoStamp implements NonSendableStampInterface {
+    private ?string $messageGroupId;
+    private ?string $messageDeduplicationId;
+
+    public function __construct(string $messageGroupId = null, string $messageDeduplicationId = null)
+    {
+        $this->messageGroupId = $messageGroupId;
+        $this->messageDeduplicationId = $messageDeduplicationId;
+    }
+
+    public function getMessageGroupId(): ?string
+    {
+        return $this->messageGroupId;
+    }
+
+    public function getMessageDeduplicationId(): ?string
+    {
+        return $this->messageDeduplicationId;
+    }
+}

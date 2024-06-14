2716b279cd9aa8aacc159a80f6cd46b2bfb2d369
Sina <fetratgharehbagh@billiger-mietwagen.de>
2024-06-14 10:12:22 +0200

    Get the eventSourceArn from $snsRecord->getTopicArn instead of getEventSubscriptionArn

--- tests/Unit/Service/Sns/SnsTransportNameResolverTest.php
+++ tests/Unit/Service/Sns/SnsTransportNameResolverTest.php
@@ -5,6 +5,7 @@ namespace Bref\Symfony\Messenger\Test\Unit\Service\Sns;
 use Bref\Event\Sns\SnsEvent;
 use Bref\Symfony\Messenger\Service\MessengerTransportConfiguration;
 use Bref\Symfony\Messenger\Service\Sns\SnsTransportNameResolver;
+use InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
 use Prophecy\Argument;
 use Prophecy\PhpUnit\ProphecyTrait;
@@ -26,9 +27,38 @@ final class SnsTransportNameResolverTest extends TestCase
         $event = new SnsEvent([
             'Records' => [
                 [
-
                     'EventSource'=>'aws:sns',
-                    'EventSubscriptionArn' => 'arn:aws:sns:us-east-1:1234567890:async',
+                    'Sns' => [
+                        'Message' => 'Test message.',
+                        'MessageAttributes' => [
+                            'Headers' => [
+                                'Type'=> 'String',
+                                'Value'=> ['Content-Type' => 'application/json'],
+                            ],
+                        ],
+                        'TopicArn' => 'arn:aws:sns:us-east-1:1234567890:async',
+                    ],
+                ],
+            ],
+        ]);
+
+        self::assertSame('async', ($transportNameResolver)($event->getRecords()[0]));
+    }
+
+    public function test_throws_exception_if_topic_arn_deos_not_exist(): void
+    {
+        $messengerTransportConfiguration = $this->prophesize(MessengerTransportConfiguration::class);
+        /** @phpstan-ignore-next-line */
+        $messengerTransportConfiguration
+            ->provideTransportFromEventSource(Argument::cetera())
+            ->willReturn('async');
+
+        $transportNameResolver = new SnsTransportNameResolver($messengerTransportConfiguration->reveal());
+
+        $eventWithMissingTopicArn = new SnsEvent([
+            'Records' => [
+                [
+                    'EventSource'=>'aws:sns',
                     'Sns' => [
                         'Message' => 'Test message.',
                         'MessageAttributes' => [
@@ -42,6 +72,7 @@ final class SnsTransportNameResolverTest extends TestCase
             ],
         ]);
 
-        self::assertSame('async', ($transportNameResolver)($event->getRecords()[0]));
+        $this->expectException(InvalidArgumentException::class);
+        ($transportNameResolver)($eventWithMissingTopicArn->getRecords()[0]);
     }
 }

9a4d547997e4d40f96fc69abf98f2630413c1906
Thibaut Cholley <thibaut.cholley@elsie-sante.fr>
2025-06-16 16:26:45 +0200

    refactor: inline SQSMessengerTransportConfiguration

--- src/Service/Sqs/SqsTransportNameResolver.php
+++ src/Service/Sqs/SqsTransportNameResolver.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace Bref\Symfony\Messenger\Service\Sqs;
 
@@ -11,18 +11,45 @@ class SqsTransportNameResolver
     private const TRANSPORT_PROTOCOL = 'sqs://';
 
     public function __construct(
-        private SQSMessengerTransportConfigurationInterface $configurationProvider
+        private array $messengerTransportsConfiguration
     ) {
     }
 
     public function __invoke(SqsRecord $sqsRecord): string
     {
-        if (!array_key_exists('eventSourceARN', $sqsRecord->toArray())) {
+        if (! array_key_exists('eventSourceARN', $sqsRecord->toArray())) {
             throw new InvalidArgumentException('EventSourceArn is missing in sqs record.');
         }
 
         $eventSourceArn = $sqsRecord->toArray()['eventSourceARN'];
+        $eventSourceWithProtocol = self::TRANSPORT_PROTOCOL . $eventSourceArn;
 
-        return $this->configurationProvider->provideTransportFromEventSource(self::TRANSPORT_PROTOCOL . $eventSourceArn);
+        foreach ($this->messengerTransportsConfiguration as $messengerTransport => $messengerOptions) {
+            $dsn = $this->extractDsnFromTransport($messengerOptions);
+
+            if ($dsn === $eventSourceWithProtocol) {
+                return $messengerTransport;
+            }
+
+            // Rebuild SQS ARN from https://sqs.eu-west-3.amazonaws.com/0123456789/messages?key=value
+            if (preg_match('/^https:\/\/sqs\.([^.]+)\.amazonaws\.com\/([^\/]+)\/([^?]+)/', (string) $dsn, $matches)) {
+                $arn = 'arn:aws:sqs:' . $matches[1] . ':' . $matches[2] . ':' . $matches[3];
+
+                if ($eventSourceWithProtocol === 'sqs://' . $arn) {
+                    return $messengerTransport;
+                }
+            }
+        }
+
+        throw new InvalidArgumentException(sprintf('No transport found for eventSource "%s".', $eventSourceWithProtocol));
+    }
+
+    private function extractDsnFromTransport(string|array $messengerTransport): string
+    {
+        if (is_array($messengerTransport) && array_key_exists('dsn', $messengerTransport)) {
+            return $messengerTransport['dsn'];
+        }
+
+        return $messengerTransport;
     }
 }
--- tests/Unit/Service/Sqs/SqsTransportNameResolverTest.php
+++ tests/Unit/Service/Sqs/SqsTransportNameResolverTest.php
@@ -1,13 +1,11 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace Bref\Symfony\Messenger\Test\Unit\Service\Sqs;
 
 use Bref\Event\Sqs\SqsRecord;
-use Bref\Symfony\Messenger\Service\Sqs\SQSMessengerTransportConfigurationInterface;
 use Bref\Symfony\Messenger\Service\Sqs\SqsTransportNameResolver;
 use InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
-use Prophecy\Argument;
 use Prophecy\PhpUnit\ProphecyTrait;
 
 final class SqsTransportNameResolverTest extends TestCase
@@ -16,13 +14,13 @@ final class SqsTransportNameResolverTest extends TestCase
 
     public function test_event_source_can_resolved_as_expected(): void
     {
-        $messengerTransportConfiguration = $this->prophesize(SQSMessengerTransportConfigurationInterface::class);
-        /** @phpstan-ignore-next-line */
-        $messengerTransportConfiguration
-            ->provideTransportFromEventSource(Argument::cetera())
-            ->willReturn('async');
+        $messengerTransportsConfiguration = [
+            'async' => [
+                'dsn' => 'sqs://arn:aws:sqs:us-east-1:1234567890:some-queue-name',
+            ],
+        ];
 
-        $transportNameResolver = new SqsTransportNameResolver($messengerTransportConfiguration->reveal());
+        $transportNameResolver = new SqsTransportNameResolver($messengerTransportsConfiguration);
 
         $event = new SqsRecord([
             'messageId' => '19dd0b57-b21e-4ac1-bd88-01bbb068cb78',
@@ -33,21 +31,46 @@ final class SqsTransportNameResolverTest extends TestCase
             ],
             'receiptHandle' => 'AQEBwJnKyrHigUMZj6rYigCgxlaS3SLy0a...',
             'eventSource' => 'aws:sqs',
-            'eventSourceARN' => 'aws:sqs:us-east-1:1234567890:some-queue-name',
+            'eventSourceARN' => 'arn:aws:sqs:us-east-1:1234567890:some-queue-name',
         ]);
 
         self::assertSame('async', ($transportNameResolver)($event));
     }
 
+    public function test_event_source_can_resolved_as_expected_with_queue_url(): void
+    {
+        $messengerTransportsConfiguration = [
+            'async' => [
+                'dsn' => 'https://sqs.us-east-1.amazonaws.com/1234567890/some-queue-name',
+            ],
+        ];
+
+        $transportNameResolver = new SqsTransportNameResolver($messengerTransportsConfiguration);
+
+        $event = new SqsRecord([
+            'messageId' => '19dd0b57-b21e-4ac1-bd88-01bbb068cb78',
+            'body' => 'Test message.',
+            'messageAttributes' => [],
+            'attributes' => [
+                'ApproximateReceiveCount' => '1',
+            ],
+            'receiptHandle' => 'AQEBwJnKyrHigUMZj6rYigCgxlaS3SLy0a...',
+            'eventSource' => 'aws:sqs',
+            'eventSourceARN' => 'arn:aws:sqs:us-east-1:1234567890:some-queue-name',
+        ]);
+
+        self::assertEquals('async', ($transportNameResolver)($event));
+    }
+
     public function test_throws_exception_if_event_source_arn_does_not_exist(): void
     {
-        $messengerTransportConfiguration = $this->prophesize(SQSMessengerTransportConfigurationInterface::class);
-        /** @phpstan-ignore-next-line */
-        $messengerTransportConfiguration
-            ->provideTransportFromEventSource(Argument::cetera())
-            ->willReturn('async');
+        $messengerTransportsConfiguration = [
+            'transport1' => [
+                'dsn' => 'sqs://arn:aws:sqs:us-east-1:0123456789:some-queue-name',
+            ],
+        ];
 
-        $transportNameResolver = new SqsTransportNameResolver($messengerTransportConfiguration->reveal());
+        $transportNameResolver = new SqsTransportNameResolver($messengerTransportsConfiguration);
 
         $eventWithMissingeventSourceARN = new SqsRecord([
             'messageId' => '19dd0b57-b21e-4ac1-bd88-01bbb068cb78',
